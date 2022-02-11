40759a4839c3823b51545bf55fb7dacbcd4e29c2
Karel <snakehead007@pm.me>
2022-02-11 16:03:28 +0100

    resolve Error: Unknown property 'class' found

--- pages/index.js
+++ pages/index.js
@@ -78,8 +78,8 @@ export default function Home() {
         }
       </div>
       <div className={"absolute w-full duration-600 transition-opacity ease-in-out bottom-2 flex justify-center"+(showAlert?" opacity-90 ":" opacity-0 ")} >
-        <div class="p-5 rounded-lg border border-gray-400 bg-slate-200 text-gray-900 flex w-1/2 justify-center">
-            <span class="mr-2">
+        <div className="p-5 rounded-lg border border-gray-400 bg-slate-200 text-gray-900 flex w-1/2 justify-center">
+            <span className="mr-2">
             <svg width={15} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M320 64h-49.61C262.1 27.48 230.7 0 192 0S121 27.48 113.6 64H64C28.65 64 0 92.66 0 128v320c0 35.34 28.65 64 64 64h256c35.35 0 64-28.66 64-64V128C384 92.66 355.3 64 320 64zM192 48c13.23 0 24 10.77 24 24S205.2 96 192 96S168 85.23 168 72S178.8 48 192 48zM336 448c0 8.82-7.178 16-16 16H64c-8.822 0-16-7.18-16-16V128c0-8.82 7.178-16 16-16h18.26C80.93 117.1 80 122.4 80 128v16C80 152.8 87.16 160 96 160h192c8.836 0 16-7.164 16-16V128c0-5.559-.9316-10.86-2.264-16H320c8.822 0 16 7.18 16 16V448z"/></svg>
             </span>
             Copied to clipboard!
